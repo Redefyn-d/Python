7b9de7b8eacf127b1298464b68fe0a90f8f1b634
Lokesh Verma <148458558+Redefyn-d@users.noreply.github.com>
2025-12-19 15:01:23 +0530

    Update and rename Q8 to Q8.py

--- ProgramAss/Activity/Q8.py
+++ ProgramAss/Activity/Q8.py
@@ -1,8 +1,8 @@
-'''f=open("random_number.txt","a")
+f=open("random_number.txt","a")
 n=input("Enter the data to append (separated by comma(,) :\n")
 f.write(n)
 f.close()
 f=open("random_number.txt","r")
 print("the updated random numbers in the file :")
 print(f.read())
-f.close()'''
+f.close()
