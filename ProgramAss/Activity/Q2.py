837d9d088b9172b9cf92bb13da6926edc68e9c12
Lokesh Verma <148458558+Redefyn-d@users.noreply.github.com>
2025-12-19 14:59:39 +0530

    Update and rename Q2 to Q2.py

--- ProgramAss/Activity/Q2.py
+++ ProgramAss/Activity/Q2.py
@@ -1,8 +1,8 @@
-'''import numpy as np
+import numpy as np
 import random
 a=[]
 for i in range(0,10):
     a.append(random.randint(0,100))
 a=np.array(a)
 print("Original array :",a)
-print("sorted array  :  ",np.sort(a))'''
+print("sorted array  :  ",np.sort(a))
